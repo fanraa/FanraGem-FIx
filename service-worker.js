54ed45fa0a2ef7117e175136d83b29318208ef1e
irfanrizkiaditri <irfanrizkiaditri@gmail.com>
2026-03-13 00:47:29 +0700

    Update service worker to cache additional files

--- service-worker.js
+++ service-worker.js
@@ -2,30 +2,27 @@ const CACHE_NAME = 'fanragem-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/manifest.json'
+  '/manifest.json',
+  '/style.css',                 // tambahkan CSS
+  '/app.js',                    // tambahkan JS
+  '/web-app-manifest-512x512.png',  
+  '/web-app-manifest-192x192.png',
+  '/icons/512x512-monochrome.png'
 ];
 
 // Install Service Worker
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Cache and return requests
+// Cache & return requests
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
   );
 });
 
@@ -33,14 +30,12 @@ self.addEventListener('fetch', event => {
 self.addEventListener('activate', event => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
+    caches.keys().then(cacheNames =>
+      Promise.all(
         cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
+          if (!cacheWhitelist.includes(cacheName)) return caches.delete(cacheName);
         })
-      );
-    })
+      )
+    )
   );
 });
